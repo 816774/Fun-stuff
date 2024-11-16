d3232c0d3e0b418b4b07f47a24cbabb7325056e9
816774 <816774.leerling@gmail.com>
2024-11-16 18:13:51 +0100

    commit

--- src/main/java/com/sophia/funstuff/FunStuff.java
+++ src/main/java/com/sophia/funstuff/FunStuff.java
@@ -1,5 +1,6 @@
 package com.sophia.funstuff;
 
+import com.sophia.funstuff.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ public class FunStuff implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
 	}
 }
